79f840d2499a1159651d623a39aa909fe0bf069a
seldacetin <64745949+seldacetin@users.noreply.github.com>
2023-04-17 13:56:03 +0300

    image_processing

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # image_processing
+
+# -Otonom Araçlarda Görüntü Işleme-
+# OpenCV, Tensorflow ve çeşitli kütüphaneleri kullanarak trafik levhalarını aracın anlıyabileceği biçimde eğitilmesi.
--- index.py
+++ index.py
@@ -4,6 +4,11 @@ import numpy as np
 import pandas as pd
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation
 
 
 def create_train_data(train_data_dir, num_classes, resize_col = 64, resize_row = 64, test_size = 0.2, random_state = 1):
@@ -40,39 +45,6 @@ def create_test_data(dataset_dir, test_info_dir, num_classes, resize_col = 64, r
     images = y_test['Path'].values
     y_test = y_test["ClassId"].values
 
-    x_train.shape
-    x_test.shape
-    x_train = []
-    x_test = []
-
-    for image in images:
-        image_path = os.path.join(dataset_dir, image)
-        image_array = cv2.imread(image_path)
-        image_array = cv2.resize(image_array, (resize_row, resize_col))
-        x_test.append(image_array)
-
-    x_test = np.array(x_test)
-    y_test = np.array(y_test)
-
-    y_test = to_categorical(y_test, num_classes)
-
-    return x_test, y_test 
-
-
-print(x_train.shape)
-
-
-
-
-
-
-"""
-def create_test_data(dataset_dir, test_info_dir, num_classes, resize_col = 64, resize_row = 64):
-    y_test = pd.read_csv(test_info_dir)
-    images = y_test['Path'].values
-    y_test = y_test["ClassId"].values
-
-    x_train = []
     x_test = []
     
     for image in images:
@@ -86,8 +58,54 @@ def create_test_data(dataset_dir, test_info_dir, num_classes, resize_col = 64, r
 
     y_test = to_categorical(y_test, num_classes)
     
-    return x_test, y_test
-x_test, y_test = create_test_data(dataset_dir, test_info_dir, num_classes, resize_col, resize_row)
-print(x_test.shape)
+    
 
-"""
+    x_train, y_train, x_test, y_test = create_test_data(dataset_dir, test_info_dir, num_classes)
+
+    
+
+    print(x_test.shape)
+    print(y_test.shape)
+
+def build_model(input_shape, num_classes,
+                padding = 'same',
+                kernel_size = (3, 3), 
+                pool_size = (2, 2), 
+                dropout_rate = 0.3):    
+    
+    model = Sequential()
+    
+    model.add(Conv2D(32, kernel_size = kernel_size, padding = padding, 
+                     input_shape = input_shape))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size = pool_size))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv2D(64, kernel_size = kernel_size, padding = padding))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size = pool_size))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv2D(128, kernel_size = kernel_size, padding = padding))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size = pool_size))
+    model.add(Dropout(dropout_rate))
+    
+    model.add(Flatten())  
+    
+    model.add(Dense(128))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Dense(num_classes, activation="softmax"))
+
+    model.compile(loss='mae', optimizer='adam', metrics= ['accuracy'])
+    
+    return model
+
+model = build_model((64,64,3),43)
+model.summary()
